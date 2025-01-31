ea6516a0ed431ebb963086e973a4a1088220ff66
Ed <11691962+Frosselly@users.noreply.github.com>
2025-02-01 00:19:21 +0200

    Moved state to parent and logic to server side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,113 @@
-import { useState } from 'react'
-import Board from './board'
-import './App.css'
+import { useState, useEffect } from "react";
+import Board from "./board";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("Press PLAY");
+
+  useEffect(() => {
+    
+  }, [])
+
+
+  const [boardOne, setBoardOne] = useState(
+    Array.from(Array(10), () => Array.from(Array(10)))
+  );
+  const [ships, setShips] = useState(new Map());
+
+
+  const [boardTwo, setBoardTwo] = useState(
+    Array.from(Array(10), () => Array.from(Array(10)))
+  );
+
+  async function play() {
+
+    
+    let data = { board: boardOne, ships: Array.from(ships)};
+
+    let res = await fetch("http://localhost:8000/play", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json', // Set the content type
+    },
+      body: JSON.stringify(data),
+    })
+    if(res.ok){
+      setText("Your turn")
+      //TODO update to show found player 2
+    }
+
+  }
+
+  function fire(r, c, b) {
+    console.log(b);
+    let data = { row: r, col: c };
+    console.log(data)
+
+    fetch("http://localhost:8000/fire", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json', // Set the content type
+    },
+      body: JSON.stringify(data),
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setBoardTwo(data.board);
+        if(data.hasWon)
+          announceWinner(data.hasWon)
+        else
+          receiveFire();
+      });
+  }
+
+  function receiveFire(){
+    setText("Enemy turn")
+    fetch("http://localhost:8000/receiveFire").then((response) => response.json())
+    .then((data) => {
+      setBoardOne(data.board);
+      if(data.hasWon)
+        announceWinner(data.hasWon)
+      else
+        setText("Your turn")
+    })
+
+  }
+
+  function announceWinner(winner) {
+    
+    setText(`${winner} WON!!!`)
+    
+  }
+
+  function updateBoard(board, ships) {
+    
+    setBoardOne(board);
+    setShips(ships)
+    
+  }
+
+  function reset() {
+    fetch("http://localhost:8000/reset")
+    setBoardTwo(Array.from(Array(10), () => Array.from(Array(10))))
+    // setBoardOne(Array.from(Array(10), () => Array.from(Array(10))))
+  }
 
   return (
     <>
-      <Board></Board>
+    <div>{text}</div>
+      <div className="game">
+        <Board owner={true} board={boardOne} onShipPlace={updateBoard} ships={ships}></Board>
+        <Board owner={false} board={boardTwo} onFire={fire}></Board>
+      </div>
+      <div className="controls">
+        <button onClick={play}>PLAY</button>
+        <button onClick={reset}>RESET</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/board.jsx
+++ client/src/board.jsx
@@ -1,141 +1,153 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function Board() {
-  const [board, setBoard] = useState(Array.from(Array(10), () => Array.from(Array(10))))
-
-  const [ships, setShips] = useState(new Map());
-
+function Board({ owner, onShipPlace, onFire, board, ships}) {
+  const colors = [
+    "oklch(0.924 0.12 95.746)",
+    "oklch(0.945 0.129 101.54)",
+    "oklch(0.91 0.096 180.426)",
+    "oklch(0.882 0.059 254.128)",
+    "oklch(0.87 0.065 274.039)",
+    "oklch(0.903 0.076 319.62)"
+  ]
 
   useEffect(() => {
-    placeShips();
-  }, [])
+    if (owner) placeShips();
+  }, []);
 
-    const Cell = ({state}) => {
-        // if(state === 'miss')
-        //     console.log( "curr state ", state )
-        return(
-            <div className={`${state} cell`} >
-                
-            </div>
-        )
-    }
-
-    function placeShips(){
-        const normal = [1,1,1,2,2,2,3,3,4,5];
-
-        let boardC = Array.from(Array(10), () => Array.from(Array(10)))
-        let shipsC = new Map();
-
-        for (let index = 0; index < normal.length;) {
-            let xR = Math.floor(Math.random() * 9)+1;
-            let yR = Math.floor(Math.random() * 9)+1;
-            
-            let length = undefined;
-            length = normal[index];
-            
-            let axis = Math.random() > 0.5 ? 'v' : 'h';
-
-            if (checkValid(boardC, xR, yR, length, axis)){
-                shipsC.set(shipsC.size, length)
-                fillShip(boardC, xR, yR, length, axis, shipsC.size-1) 
-                index++;
-            }
-        }
-
-        setBoard(boardC)
-        setShips(shipsC)
-
-    }
-
+  const Cell = ({ state }) => {
     
 
-    function checkValid(boardC, x, y, length, axis)
-    {
-      x--;
-      y--;
-      if (x < 0  || y < 0 || 10 <= y ||10 <= x ) return false;
-      if (axis == "h" && 10 <= x + length) return false;
-      if (axis == "v" && 10 <= y + length) return false;
-      if (axis.length !== 1 || !axis.match(/[hv]/)) return false;
-  
-      let placed = 0;
-      for (let row = 0; row < 10; row++) {
-        for (let col = 0; col < 10; col++) {
-          if (
-            (axis == "h" && row === y && x <= col && col < x + length) ||
-            (axis == "v" && col === x && y <= row && row < y + length)
-          ) {
-            if (boardC[row][col] !== undefined) return false;
-            placed++;
-            if(placed >= length) break;
-          }
-        }
-        if(placed >= length) break;
+    let data = state;
+    if (Array.isArray(data)){
+
+      return <div 
+      style={
+        {backgroundColor : ships.get(data[0]).color}
       }
-      return true;
+      className={`${data[0]} cell`}></div>;
+    } 
+
+    return <div className={`${data} cell`}></div>;
+  };
+
+  function placeShips() {
+    const normal = [1, 1, 1, 2, 2, 2, 3, 3, 4, 5];
+
+    let boardC = Array.from(Array(10), () => Array.from(Array(10)));
+    let shipsC = new Map();
+
+    for (let index = 0; index < normal.length; ) {
+      let xR = Math.floor(Math.random() * 9) + 1;
+      let yR = Math.floor(Math.random() * 9) + 1;
+
+      let length = undefined;
+      length = normal[index];
+
+      let axis = Math.random() > 0.5 ? "v" : "h";
+
+      if (checkValid(boardC, xR, yR, length, axis)) {
+        shipsC.set(shipsC.size, { len: length, hits: [], color:colors[Math.floor(Math.random() * 6)] });
+        fillShip(boardC, xR, yR, length, axis, shipsC.size - 1);
+        index++;
+      }
     }
+    onShipPlace(boardC, shipsC);
+    // setBoard(boardC)
+    // setShips(shipsC);
+  }
 
-    function fillShip(boardC, x, y, length, axis, ship) {
+  function checkValid(boardC, x, y, length, axis) {
+    x--;
+    y--;
+    if (x < 0 || y < 0 || 10 <= y || 10 <= x) return false;
+    if (axis == "h" && 10 <= x + length) return false;
+    if (axis == "v" && 10 <= y + length) return false;
+    if (axis.length !== 1 || !axis.match(/[hv]/)) return false;
 
-        x--;
-        y--;
-        let placed = 0;
-        for (let row = 0; row < 10; row++) {
-          for (let col = 0; col < 10; col++) {
-            if (
-              (axis == "h" && row === y && x <= col && col < x + length) ||
-              (axis == "v" && col === x && y <= row && row < y + length)
-            ) {
-                boardC[row][col] = ship;
-              placed++;
-              if(placed >= length) break;
-            }
-          }
-          if(placed >= length) break;
+    let placed = 0;
+    for (let row = 0; row < 10; row++) {
+      for (let col = 0; col < 10; col++) {
+        if (
+          (axis == "h" && row === y && x <= col && col < x + length) ||
+          (axis == "v" && col === x && y <= row && row < y + length)
+        ) {
+          if (boardC[row][col] !== undefined) return false;
+          placed++;
+          if (placed >= length) break;
         }
-        return true;
+      }
+      if (placed >= length) break;
     }
+    return true;
+  }
 
-    function checkCell(row, col){
-        let copy = structuredClone(board);
-        const key = board[row][col];
-        if(ships.has(key)){
-            copy[row][col] = 'hit'
-
-            let shipsUpdate = structuredClone(ships);
-            shipsUpdate.set(key, ships.get(key) - 1)
-            setShips(shipsUpdate)
+  function fillShip(boardC, x, y, length, axis, ship) {
+    x--;
+    y--;
+    let placed = 0;
+    for (let row = 0; row < 10; row++) {
+      for (let col = 0; col < 10; col++) {
+        if (
+          (axis == "h" && row === y && x <= col && col < x + length) ||
+          (axis == "v" && col === x && y <= row && row < y + length)
+        ) {
+          boardC[row][col] = [ship, "ship"];
+          placed++;
+          if (placed >= length) break;
         }
-        else{
-            copy[row][col] = 'miss'
-        }
-        console.log(ships)
-        setBoard(copy)
+      }
+      if (placed >= length) break;
     }
+    return true;
+  }
+  //TODO change to server side async Fire
+  function checkCell(row, col) {
+    if (owner) return;
+
+    // let boardCopy = structuredClone(board);
+    // const key = board[row][col];
+    // if (ships.has(key)) {
+    //   boardCopy[row][col] = "hit";
+
+    //   let shipsUpdate = structuredClone(ships);
+    //   let shipData = ships.get(key);
+    //   shipData.hits.push([row, col]);
+    //   shipsUpdate.set(key, shipData);
+    //   setShips(shipsUpdate);
+
+    //   if (shipData.hits.length >= shipData.len) {
+    //     destroyShip(boardCopy, shipData.hits);
+    //   }
+    // } else {
+    //   boardCopy[row][col] = "miss";
+    // }
+    onFire(row, col, board);
+  }
 
   return (
     <>
-    <table>
-    <tbody className='board'>
-        {
-            board.map((row, i) => (
-                <tr key={`${i}`}>
-                    {
-                       row.map((col, j)=>(
-                        <td key={`${i}+${j}`} value={col} onClick={() => checkCell(i, j)}>
-                            <Cell state={col}></Cell>
-                        </td>
-                    )) 
-                    }
-                    
-                </tr>
-            ))
-        }
-    </tbody>
-    </table>
+      <table>
+        <tbody className="board">
+          {board.map((row, i) => (
+            <tr key={`${i}`}>
+              {row.map((col, j) => (
+                <td
+                  key={`${i}+${j}`}
+                  value={col}
+                  onClick={() => checkCell(i, j)}
+                >
+                  <Cell state={col}></Cell>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
-  )
+  );
 }
 
-export default Board
+export default Board;
+
+// function fireCell(row, col) {}
