194b407324e9e16b4389250c2ac6a45b2c66b679
Ed <11691962+Frosselly@users.noreply.github.com>
2025-02-03 02:59:06 +0200

    Moved README

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,13 @@
-import { createContext, StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-const UserContext = createContext(null)
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-  <UserContext.Provider>
+  
     <App />
-  </UserContext.Provider>
+  
   // </StrictMode>,
 )
